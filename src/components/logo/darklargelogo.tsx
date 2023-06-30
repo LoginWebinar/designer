4126ab8eae01fb37432caeea1d88befb80f8ee4f
Rob Helmstetter <rob@icecodeproductions.com>
2023-06-30 16:24:21 -0400

    stoped with <image and using <img

--- src/components/logo/darklargelogo.tsx
+++ src/components/logo/darklargelogo.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 const disenoLogoDark = "https://firebasestorage.googleapis.com/v0/b/diseno-5b992.appspot.com/o/public%2Flogos%2Fdisenoaicenterfordarktheme.webp?alt=media&token=4df5df3a-2635-4275-8529-0dec8d3302b7"
 
 function LargeDarkLogo(){
-    return <Image src={disenoLogoDark} width={642} height={200} alt="Diseno Logo"/>;
+    // return <Image src={disenoLogoDark} width={642} height={200} alt="Diseno Logo"/>;
+    return <img src={disenoLogoDark} className="w-full h-auto" alt="Diseno Logo" />;
 }
 
 export default LargeDarkLogo;
--- src/components/logo/darksmalllogo.tsx
+++ src/components/logo/darksmalllogo.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 const disenoLogoDark = "https://firebasestorage.googleapis.com/v0/b/diseno-5b992.appspot.com/o/public%2Flogos%2Fdisenoaicenterfordarktheme.webp?alt=media&token=4df5df3a-2635-4275-8529-0dec8d3302b7"
 
 function SmallDarkLogo(){
-    return <Link href="https://designer.diseno.ai" passHref><Image src={disenoLogoDark} width={100} height={75} alt="Diseno Logo"  /></Link>;
+    // return <Link href="https://designer.diseno.ai" passHref><Image src={disenoLogoDark} width={100} height={75} alt="Diseno Logo"  /></Link>;
+    return <img src={disenoLogoDark} className="w-full h-auto" alt="Diseno Logo" />;
 }
 
 export default SmallDarkLogo;

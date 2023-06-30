dadb1abd5faa2deeea8775917b075050741127bb
Rob Helmstetter <rob@icecodeproductions.com>
2023-06-30 16:07:58 -0400

    Steps for Image Loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com']
+    domains: ['firebasestorage.googleapis.com'],
+    loader: 'custom',
+    loaderFile: './imageLoader.ts',
   },
 }
 
